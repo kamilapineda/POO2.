d39f15f59b785ddf64dacd3e8f7ad66228dbc26f
Kamila :P <80299068+kamilapineda@users.noreply.github.com>
2022-06-09 20:01:07 -0500

    Delete FinalPoo2_KamilaP directory

--- FinalPoo2_KamilaP/texto/reordenar.py
+++
@@ -1,9 +0,0 @@
-def palindromo(texto):
-    texto=texto.replace(" ","")
-    tam=len(texto)
-    cont=0
-    for i in reversed(range(0,tam)):
-        if texto[i].lower()!=texto[cont].lower():
-            return False
-        cont=cont+1
-    return True 
